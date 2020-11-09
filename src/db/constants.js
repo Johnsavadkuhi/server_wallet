d11b49e7a7a3e1f1999ead99ef80ac6434c29c01
Johnsavadkuhi <physitime@gmail.com>
2020-11-10 00:39:09 +0330

    update

--- src/db/constants.js
+++ src/db/constants.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 const   CONNECTION_STRING = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.vhani.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
-
+console.log(process.env.USER_NAME , CONNECTION_STRING); 
 module.exports= CONNECTION_STRING 
