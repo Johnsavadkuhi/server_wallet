8f3d9dd6adafce5490ad8573c17e3207e5fda1e1
Johnsavadkuhi <physitime@gmail.com>
2020-11-10 00:38:55 +0330

    update

--- src/server.js
+++ src/server.js
@@ -1,35 +1,55 @@
-require('dotenv').config()
-const {ApolloServer  , gql } = require('apollo-server') 
+const { ApolloServer, gql } = require("apollo-server");
 
-
-const books = [
+const users = [
     {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+      id: '1',
+      name: 'Elizabeth Bennet'
     },
     {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+      id: '2',
+      name: 'Fitzwilliam Darcy'
+    }
+  ]
 
 const typeDefs = gql`
 
-type Book {
-    title:String 
-    author: String 
+type User {
+    id:ID! 
+    name:String!
 }
 
-type Query {
-    books:[Book]
-}
+ type Query{
+     numberSix:Int!
+     hello:String!
+     user(id:ID!):User
+  }
 
-`
-const resolvers = {Query : {
-    books: () =>books 
-}}
-const server = new ApolloServer({typeDefs , resolvers , playground:true , introspection:true}  ) ; 
 
-server.listen().then(({url})=>{
-    console.log("Server ready at " + url );
-})
+
+
+`;
+const resolvers = {
+   
+  Query: {
+      numberSix : () => 6  , 
+      hello:  () => "hello world ",
+
+      user (parent , args , context , info  ){
+
+        return users.find(user => user.id === args.id) 
+      }
+},
+
+};
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  
+  playground: true,
+  introspection: true,
+  tracing:true 
+});
+
+server.listen().then(({ url }) => {
+  console.log("Server ready at " + url);
+});
